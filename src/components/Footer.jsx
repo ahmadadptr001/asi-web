cb9994201ec2c35cb64b6754644722e498ecd7b2
serrie <mohammadzidane058@gmail.com>
2025-05-19 20:11:07 +0700

    add: footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <>
+            <div className="bg-yellow-500 p-4 text-md text-slate-900 text-center">&copy; {new Date().getFullYear()} Asi-web. All right reserved.</div>
+        </>
+    )
+}
